bb9433fe6c4e2c0eb9e293f304b74b824c0fdaa4
Gurleen Singh <gurleen@twitnit.me>
2019-09-17 16:34:05 -0400

    New dashboard panels

--- extension/index.js
+++ extension/index.js
@@ -7,12 +7,19 @@ module.exports = function (nodecg) {
 	// HOME LTH DATA
 	var currentPath = process.cwd();
 	console.log(currentPath);
-	let rawData = fs.readFileSync('bundles/dutv-graphics-package/extension/team-info/drexel/mens_lacrosse.json');
+	let rawData = fs.readFileSync('bundles/dutv-graphics-package/extension/team-info/drexel/mens_soccer.json');
 	let homeTeam = JSON.parse(rawData);
 	const homeTeamRep = nodecg.Replicant('homeTeam');
 	homeTeamRep.value = homeTeam;
-	nodecg.log.info('Home team set to ' + homeTeam.teamInfo.schoolName + ' ' + homeTeam.teamInfo.teamName);
 
+	/* AWAY LTH DATA
+	var currentPath = process.cwd();
+	console.log(currentPath);
+	let rawData = fs.readFileSync('bundles/dutv-graphics-package/extension/team-info/other/mens_soccer.json');
+	let awayTeam = JSON.parse(rawData);
+	const homeTeamRep = nodecg.Replicant('awayTeam');
+	awayTeamRep.value = awayTeam;
+	*/
 
 	// TIME/SCORE DATA
 	const liveData = 'bundles/dutv-graphics-package/extension/livedata.json'
@@ -22,5 +29,17 @@ module.exports = function (nodecg) {
 		liveDataRep.value = JSON.parse(fs.readFileSync(liveData));
 		// nodecg.log.info('New score data!');
 		// nodecg.log.info(liveDataRep.value.clockStr);
-	})
+	});
+
+	const imageList = nodecg.Replicant('imageList');
+	var tempList = [];
+	fs.readdir(('bundles/dutv-graphics-package/graphics/img'), (err, files) => {
+		files.forEach(file => {
+			if(file.endsWith('.png')) {
+				tempList.push(file);
+				console.log(file);
+			}
+		});
+		imageList.value = tempList;
+	});
 };
